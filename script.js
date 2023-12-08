4d91b56d0e267d873e03fe061720b73024bcba40
jwwals1 <joe_walsh59@yahoo.com>
2023-12-08 13:29:18 -0600

    added library function to the console

--- script.js
+++ script.js
@@ -12,13 +12,35 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-    let title = prompt('What is the title')
-    let author = prompt('Who is the author')
-    let pages = prompt('how many pages')
-    let read = prompt('have you read it ')
+    let title = prompt('title?')
+    let author = prompt('author?')
+    let pages = prompt('pages?')
+    let read = prompt('read?')
+    // let title = document.querySelector('title').value;
+    // let author = document.querySelector('author').value;
+    // let pages = document.querySelector('pages').value
+    // let read = document.querySelector('read').value
     let newBook = new Book(title, author, pages, read)
     myLibrary.push(newBook)
+    library()
 }
 
 const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, 'not read yet')
 // console.log(theHobbit.info())
+
+
+function library() {
+    const books = document.getElementById('library')
+    books.innerHTML = ""
+    for (let i = 0; i < myLibrary.length; i++) {
+        let book = myLibrary[i];
+        let bookEl = document.createElement("div");
+        bookEl.innerHTML = `${book.title} by ${book.author}, ${book.pages} pages, ${book.read}`;
+        books.appendChild(bookEl)
+    }
+}
+
+
+
+
+library()
